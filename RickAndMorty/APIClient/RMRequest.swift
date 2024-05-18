bcfe1f487dd2bba2af248cacf0827bfc67e3ca77
Wilbert Devin Wijaya <54884570+wilbertdevin@users.noreply.github.com>
2024-05-18 14:49:14 +0700

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Wilbert Devin Wijaya on 18/05/24.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+    
+}
