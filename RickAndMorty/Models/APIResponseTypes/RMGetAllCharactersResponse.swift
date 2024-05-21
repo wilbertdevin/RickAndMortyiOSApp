45a49ee9f08e998d169fd7f0fb4235f072a4a24e
Wilbert Devin Wijaya <54884570+wilbertdevin@users.noreply.github.com>
2024-05-21 23:41:12 +0700

    Add API Call

---
+++ RickAndMorty/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
@@ -0,0 +1,20 @@
+//
+//  RMGetAllCharactersResponse.swift
+//  RickAndMorty
+//
+//  Created by Wilbert Devin Wijaya on 21/05/24.
+//
+
+import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [RMCharacter]
+}
